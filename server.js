7e0cf6c567c4765f596a13212686b7a3cda65a67
shvn19 <shvn19@gmail.com>
2023-03-11 19:09:16 +0700

    ss

---
+++ server.js
@@ -0,0 +1,53 @@
+// call all the required packages
+const express = require('express')
+const multer = require('multer');
+const app = express();
+
+const PORT = 5000;
+
+app.use(express());
+
+// SET STORAGE
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+var upload = multer({ storage: storage })
+
+//CREATE EXPRESS APP
+
+ 
+// ROUTES
+app.get('/',function(req,res){
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
+  const file = req.file
+  console.log('#1.1 come here')
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  console.log('#1 file taken')
+  res.send(file)
+})
+
+//Uploading multiple files
+app.post('/uploadmultiple', upload.array('myFiles', 12), (req, res, next) => {
+  const files = req.files
+  if (!files) {
+    const error = new Error('Please choose files')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  res.send(files)
+})
+ 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
